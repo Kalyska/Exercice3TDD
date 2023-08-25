87816c455979e2a8402e9a5815b80769d79e0c0d
Clemence <penin.clemence@gmail.com>
2023-08-25 14:21:17 +0200

    création des projets : + bibliothèque de classe RechercheVille + projet Ms Test associé

---
+++ ex3TDD/ex3TDD.Core/RechercheVille.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex3TDD.Core
+{
+    public class RechercheVille
+    {
+        private List<String>? villes;
+
+        public List<String> Rechercher(String mot)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
